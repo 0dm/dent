4875fec17548f2b8b33299db3ec05314675e0059
0dm <ashah775@outlook.com>
2020-10-19 22:44:09 -0400

    Update GFX.h

--- GFX/GFX.h
+++ GFX/GFX.h
@@ -5,27 +5,27 @@
 #else
   #include <thread>
 #endif
-#include "SDL2/SDL.h"
+#include <SDL2/SDL.h>
 #include "../CPU/CPU.h"
 
 #define keysym key.keysym.sym
 
 static uint8_t keyPad[0x10] = {
-        SDLK_x,
         SDLK_1,
         SDLK_2,
         SDLK_3,
+        SDLK_4,
         SDLK_q,
         SDLK_w,
         SDLK_e,
+        SDLK_r,
         SDLK_a,
         SDLK_s,
         SDLK_d,
-        SDLK_z,
-        SDLK_c,
-        SDLK_4,
-        SDLK_r,
         SDLK_f,
+        SDLK_z,
+        SDLK_x,
+        SDLK_c,
         SDLK_v,
 };
 
